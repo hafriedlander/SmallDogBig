d140c977bcf9bf361515ccd5f1c4274169828792
Hamish Friedlander <hafriedlander@gmail.com>
2022-09-08 18:21:46 +1200

    Move examples out of default inputs directory, and add examples of other upscaler models

---
+++ build_examples.cmd
@@ -0,0 +1,7 @@
+python smalldogbig.py --in_path examples --w 0.2 --bg_upsampler swinir
+python smalldogbig.py --in_path examples --w 0.2 --bg_upsampler swinir_x2
+python smalldogbig.py --in_path examples --w 0.2 --bg_upsampler realesrgan
+python smalldogbig.py --in_path examples --w 0.2 --bg_upsampler realesrgan_anime
+python smalldogbig.py --in_path examples --w 0.2 --bg_upsampler realesrgan_x2
+python smalldogbig.py --in_path examples --w 0.2 --bg_upsampler none
+python smalldogbig.py --in_path examples --bg_upsampler none --no_face_correction
